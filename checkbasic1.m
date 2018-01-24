bfdbd0d75165104d98cd45f24e830cbb6e0b38c2
WilhelmLund <35770378+WilhelmLund@users.noreply.github.com>
2018-01-24 15:11:32 +0100

    adding checkbasic (files for handin 1)

---
+++ checkbasic1.m
@@ -0,0 +1,37 @@
+function [tableau,x,basic,feasible,optimal]=checkbasic1(A,b,c,basicvars);
+% [x,basic,optimal,feasible]=checkbasic1(A,b,c,basicvars),
+% INPUT:    A - mxn matrix
+%           b - mx1 matrix
+%           c - nx1 matrix
+%           basicvars - list of m indices between 1 and n.
+% OUTPUT:
+%   tableau     - the (m+1) x (n+1) matrix representing the simplex tableau
+%               (skip the column corresponding to the objective function z)
+%   x           - nx1 matrix. The basic solution corresponding
+%               to basic variables basicvars.
+%   basic       - 1 if x is a basic solution
+%   optimal     - 1 if x is an optimal solution
+%   feasible    - 1 if x is a feasible solution
+% to the LP-problem in canonical form
+% max z = c’*x
+% subject to A*x=b, x>=0
+
+m = size(A, 1);
+n = size(A, 2);
+
+nonbasic = setdiff(1:size(A,2), basicvars);
+
+Ab = A(:,basicvars);
+An = A(:,nonbasic);
+
+cb = c(basicvars);
+cn = c(nonbasic);
+
+tableau = [inv(Ab)*An eye(m) inv(Ab)*b; cb'*inv(Ab)*An-cn' zeros(1,m) cb'*inv(Ab)*b];
+        
+x = [zeros(n-m,1); tableau(1:m,end)];
+basic = 1;
+feasible = all(x(1:n) >=0);
+optimal = all(tableau(end,1:(n-m)) >= 0);
+
+
---
+++ test_checkbasic1.m
@@ -0,0 +1,18 @@
+
+% Script for testing the tableau.
+A = [1 1 1 0 0;1 0 0 1 0;8 20 0 0 1];
+b = [1;3/4;10];
+c = [2;1;0;0;0];
+basicvars=[3 4 5];
+[tableau,x,basic,feasible,optimal]=checkbasic1(A,b,c,basicvars);
+disp('Problem 2a');
+disp(basicvars);
+tableau
+basicvars=[3 1 5];
+[tableau,x,basic,feasible,optimal]=checkbasic1(A,b,c,basicvars);
+disp('Problem 2b');
+tableau
+basicvars=[3 1 2];
+[tableau,x,basic,feasible,optimal]=checkbasic1(A,b,c,basicvars);
+disp('Problem 2c');
+tableau
